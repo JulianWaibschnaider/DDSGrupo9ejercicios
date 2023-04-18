d26e573ed64271a397ff66ea6580fdb7fb65f366
jwaibschnaider <jwaibschnaider@frba.utn.edu.ar>
2023-04-18 19:54:58 -0300

    singleton en biblio

--- ej31/src/ej31/Biblioteca.java
+++ ej31/src/ej31/Biblioteca.java
@@ -5,4 +5,19 @@ import java.util.ArrayList;
 public class Biblioteca {
 	private ArrayList<Libro> libros;
 	private ArrayList<Lector> lectores;
+
+	private static Biblioteca _biblioteca;
+
+	private Biblioteca() {
+	}
+
+	public static Biblioteca getInstance() {
+		if (_biblioteca == null) {
+			_biblioteca = new Biblioteca();
+			return _biblioteca;
+		} else {
+			return _biblioteca;
+		}
+	}
+
 }
